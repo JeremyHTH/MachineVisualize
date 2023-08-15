1d40da43b6414338dffb61abbbb3dcbd018f6e02
JeremyHTH <jeremy.hung56@gmail.com>
2023-08-15 20:14:33 +0800

    Add Component

--- Componment.py
+++ Componment.py
@@ -1,16 +1,55 @@
+from dataclasses import dataclass
+
+@dataclass
+class  Coordinate():
+    x : float   = 0.0
+    y : float   = 0.0
+    z : float   = 0.0
+
+    def __str__(self) -> str:
+        Status = f'x: {self.x:.2f}  y: {self.y:.2f} z: {self.z:.2f}'
+        return Status
+
+@dataclass
+class  RotationalCoordinate():
+    x : float   = 0.0
+    t : float   = 0.0
+
+    def __str__(self) -> str:
+        Status = f'x: {self.x:.2f}  t: {self.t:.2f}'
+
+
 class MovingComponent():
 
     def __init__(self, *arg, **kwarg) -> None:
         self.Name = "" if (not "Name" in kwarg) else kwarg['Name']
-        self.x = 0 if (not "x" in kwarg) else kwarg['x']
-        self.y = 0 if (not "y" in kwarg) else kwarg['y']
-        self.z = 0 if (not "z" in kwarg) else kwarg['z']
-        self.t = 0 if (not "t" in kwarg) else kwarg['t']
+        # self.Width = 100 if (not "Width" in kwarg) else kwarg['Width']
+        # self.Height = 100 if (not "Height" in kwarg) else kwarg['Height']
+        
+        self.Dimension = Coordinate(x = 100.0, y = 100.0, z = 100)
+        self.CurrPos =  Coordinate()
+        self.MaxPos = Coordinate()
+        self.MinPos = Coordinate()
+        self.SIToPixRatio = Coordinate()
+
+        if ("CurrPos" in kwarg and isinstance(kwarg["CurrPos"], Coordinate)):
+            self.CurrPos = kwarg["CurrPos"]
+        
+        if ("MaxPos" in kwarg and isinstance(kwarg["MaxPos"], Coordinate)):
+            self.MaxPos = kwarg["MaxPos"]
+
+        if ("MinPos" in kwarg and isinstance(kwarg["MinPos"], Coordinate)):
+            self.MinPos = kwarg["MinPos"]
+
+        if ("SIToPixRatio" in kwarg and isinstance(kwarg["SIToPixRatio"], Coordinate)):
+            self.SIToPixRatio = kwarg["SIToPixRatio"]
 
     def __str__(self) -> str:
-        Status = f'Name : {self.Name} \nx: {self.x} y: {self.y} z: {self.z} t: {self.t}'
+        Status = f'Name: {self.Name} Width: {self.Width}    Height: {self.Height}\nCurrPos:     {str(self.CurrPos)} \nMax:      {self.MaxPos}\nMin:     {self.MinPos}\nSIToPixRatio:    {self.SIToPixRatio}'
         return Status
     
 if __name__ == '__main__':
-    Eye = MovingComponent(Name = "Hand", x = 10, y = 20, z = 30)
-    print(Eye)
+    # Eye = MovingComponent(Name = "Hand", x = 10, y = 20, z = 30)
+    Head = MovingComponent(CurrPos = Coordinate(x = 5.0))
+    leg = MovingComponent()
+    print(Head)
--- MainUI.py
+++ MainUI.py
@@ -2,62 +2,139 @@ import sys,time,os
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap, QImage, QPainter, QPen, QBrush, QColor
 from PyQt5 import QtGui
-from PyQt5.QtCore import Qt
-from Componment import MovingComponent
+from PyQt5.QtCore import Qt, QRect
+from Componment import MovingComponent, Coordinate
 import subprocess
 import platform
+import csv
 
-MID_WINDOW_WIDTH = 400
-MID_WINDOW_HEIGHT = 300
-LOW_WINDOW_WIDTH = 400
-LOW_WINDOW_HEIGHT = 300
+SIDE_WINDOW_WIDTH = 600
+SIDE_WINDOW_HEIGHT = 900
+MID_WINDOW_WIDTH = 600
+MID_WINDOW_HEIGHT = 450
+LOW_WINDOW_WIDTH = 600
+LOW_WINDOW_HEIGHT = 450
+VIEW_SETTING = {'Side': ("Head", "Eye", "MegaEye", "LeftLeg", "RightLeg"), 'Mid': ("Eye", "MegaEye", "Arm"), 'Bottom': ("LeftLeg", "RightLeg")}
+
 
 class CenterWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-
+        self.ComponentList = {}
+        self.CreateComponent()
+        self.ComponentDrawer = {'Pen': QPen(),'Brush': QBrush()}
+        self.ComponentDrawer['Pen'].setWidth(1)
+        self.ComponentDrawer['Pen'].setColor(QColor("#420803"))
+        self.ComponentDrawer['Brush'].setColor(QColor("#0a0838"))
+        self.ComponentDrawer['Brush'].setStyle(Qt.Dense1Pattern)
+    
+        self.TextDrawer = {'Pen': QPen(),'Brush': QBrush()}
+        self.TextDrawer['Pen'].setWidth(1)
+        self.TextDrawer['Pen'].setColor(QColor("#FFFFFF"))
+        self.TextDrawer['Brush'].setColor(QColor("#FFFFFF"))
+        self.TextDrawer['Brush'].setStyle(Qt.Dense1Pattern)
         self.init_UI()
         
-
+        
+    def CreateComponent(self):
+        self.ComponentList : dict[str, MovingComponent] = {}
+        with open("Configure/ComponentConfig.csv", 'r') as CSVFile:
+            CSVRows = csv.reader(CSVFile)
+            for Line in CSVRows:
+                if (len(Line) >= 2 and Line[1] == '1'):
+                    self.ComponentList[Line[0]] = MovingComponent(Name = Line[0])
+                    if (len(Line) == 5):
+                        self.ComponentList[Line[0]].Dimension.x = eval(Line[2])
+                        self.ComponentList[Line[0]].Dimension.y = eval(Line[3])
+                        self.ComponentList[Line[0]].Dimension.z = eval(Line[4])
+        
+        # print(self.ComponentList)
     def init_UI(self):
         self.layout = QGridLayout(self)
 
-
+        self.SideLayerDisplay = QLabel(self)
+        canvas = QPixmap(SIDE_WINDOW_WIDTH, SIDE_WINDOW_HEIGHT)
+        self.SideLayerDisplay.setPixmap(canvas)
+        self.layout.addWidget(self.SideLayerDisplay, 0, 0, 2, 1)
 
         self.MidLayerDisplay = QLabel(self)
         canvas = QPixmap(MID_WINDOW_WIDTH, MID_WINDOW_HEIGHT)
         self.MidLayerDisplay.setPixmap(canvas)
-        self.layout.addWidget(self.MidLayerDisplay, 0, 0, 1, 1)
+        self.layout.addWidget(self.MidLayerDisplay, 0, 1, 1, 1)
 
-        self.DrawMidLayComponent()
+        self.BottomLayerDisplay = QLabel(self)
+        canvas = QPixmap(LOW_WINDOW_WIDTH, LOW_WINDOW_HEIGHT)
+        self.BottomLayerDisplay.setPixmap(canvas)
+        self.layout.addWidget(self.BottomLayerDisplay, 1, 1, 1, 1)
 
-        self.LowLayerDisplay = QLabel(self)
+        if ('Eye' in self.ComponentList):
+            self.ComponentList['Eye'].CurrPos = Coordinate(200, 200, 450)
+        if ('MegaEye' in self.ComponentList):    
+            self.ComponentList['MegaEye'].CurrPos = Coordinate(100, 100, 450)
+        if ('Head' in self.ComponentList):    
+            self.ComponentList['Head'].CurrPos = Coordinate(250, 100, 0)
+        if ('LeftLeg' in self.ComponentList):    
+            self.ComponentList['LeftLeg'].CurrPos = Coordinate(100, 100, 700)
+        if ('RightLeg' in self.ComponentList):    
+            self.ComponentList['RightLeg'].CurrPos = Coordinate(500, 100, 700)
         
+        self.DrawSideLayerComponent()
+        self.DrawMidLayComponent()
+        self.DrawBottomLayComponent()
+
         
         self.setLayout(self.layout)
     
+    def DrawSideLayerComponent(self):
+        self.SideLayerDisplay.pixmap().fill(Qt.black)
+        painter = QPainter(self.SideLayerDisplay.pixmap())
+        
+        for Component in VIEW_SETTING["Side"]:
+            if (Component in self.ComponentList):
+                Curr = self.ComponentList[Component]
+                Rect = QRect(Curr.CurrPos.x, Curr.CurrPos.z, Curr.Dimension.x, Curr.Dimension.z)
+                painter.setPen(self.ComponentDrawer['Pen'])
+                painter.setBrush(self.ComponentDrawer['Brush'])
+                painter.drawRect(Rect)
+                painter.setPen(self.TextDrawer['Pen'])
+                painter.setBrush(self.TextDrawer['Brush'])
+                print(Curr.Name)
+                painter.drawText(Rect, Qt.AlignCenter, Curr.Name)
+
     def DrawMidLayComponent(self):
         self.MidLayerDisplay.pixmap().fill(Qt.black)
-        painter = QPainter(self.MidLayerDisplay.pixmap())
-        pen = QPen()
-        pen.setWidth(8)
-        pen.setColor(QColor("#0c5206"))
-        painter.setPen(pen)
-
-        painter.drawRect(0, 0, MID_WINDOW_WIDTH, MID_WINDOW_HEIGHT)
-
-        pen.setWidth(1)
-        pen.setColor(QColor("#420803"))
-        painter.setPen(pen)
-
-        brush = QBrush()
-        brush.setColor(QColor("#0a0838"))
-        brush.setStyle(Qt.Dense1Pattern)
-        painter.setBrush(brush)
-
-        painter.drawRect(100,100,50,50)
+        painter = QPainter(self.MidLayerDisplay.pixmap())      
         
+
+        for Component in VIEW_SETTING["Mid"]:
+            if (Component in self.ComponentList):
+                Curr = self.ComponentList[Component]
+                Rect = QRect(Curr.CurrPos.x, Curr.CurrPos.y, Curr.Dimension.x, Curr.Dimension.y)
+                painter.setPen(self.ComponentDrawer['Pen'])
+                painter.setBrush(self.ComponentDrawer['Brush'])
+                painter.drawRect(Rect)
+                painter.setPen(self.TextDrawer['Pen'])
+                painter.setBrush(self.TextDrawer['Brush'])
+                print(Curr.Name)
+                painter.drawText(Rect, Qt.AlignCenter, Curr.Name)
+    
+    def DrawBottomLayComponent(self):
+        self.BottomLayerDisplay.pixmap().fill(Qt.black)
+        painter = QPainter(self.BottomLayerDisplay.pixmap())      
+        
+
+        for Component in VIEW_SETTING["Bottom"]:
+            if (Component in self.ComponentList):
+                Curr = self.ComponentList[Component]
+                Rect = QRect(Curr.CurrPos.x, Curr.CurrPos.y, Curr.Dimension.x, Curr.Dimension.y)
+                painter.setPen(self.ComponentDrawer['Pen'])
+                painter.setBrush(self.ComponentDrawer['Brush'])
+                painter.drawRect(Rect)
+                painter.setPen(self.TextDrawer['Pen'])
+                painter.setBrush(self.TextDrawer['Brush'])
+                print(Curr.Name)
+                painter.drawText(Rect, Qt.AlignCenter, Curr.Name)
 
     def close(self) -> bool:
         self.FirstRow.close()
